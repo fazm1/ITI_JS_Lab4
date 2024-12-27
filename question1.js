e35393013f795c710851d00d8cdc2775bcf671a3
fazm1 <farid.azzam.2001@gmail.com>
2024-12-27 19:08:35 +0200

    third commit

---
+++ question1.js
@@ -0,0 +1,27 @@
+var interval = null;
+var images = ["images/img1.jpg", "images/img2.jpg", "images/img3.jpg", "images/img4.jpg"]
+var i = 0;
+function hover() {
+    interval = setInterval(function () {
+
+        document.getElementById("slideshow").src = images[i]
+        i++;
+        if (i > 3) {
+            i = 0;
+        }
+    }
+        , 1000)
+}
+
+function leave() {
+    clearInterval(interval);
+    interval = null;
+    document.getElementById("slideshow").src = "images/img1.jpg";
+}
+
+
+
+
+
+
+
